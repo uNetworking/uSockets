d8362a74b3105a7f6e51302650ef7b6d6762e479
Alex Hultman <alexhultman@gmail.com>
2018-06-24 05:03:16 +0200

    Bring back the 50mb streaming test

--- src/main.c
+++ src/main.c
@@ -1,225 +1,94 @@
 #include "libusockets.h"
 
-#define USE_SSL
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+struct app_http_socket {
+    int offset;
+};
+
+struct app_http_context {
+
+};
 
 // small response
-// fast in ssl
-char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 512\r\n\r\n";
-int largeHttpBufSize = sizeof(largeBuf) + 512 - 1;
+//char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 512\r\n\r\n";
+//int largeHttpBufSize = sizeof(largeBuf) + 512 - 1;
 
-// fast in ssl
 //char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 5120\r\n\r\n";
 //int largeHttpBufSize = sizeof(largeBuf) + 5120 - 1;
 
-// fast in ssl
 //char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 51200\r\n\r\n";
 //int largeHttpBufSize = sizeof(largeBuf) + 51200 - 1;
 
-// fast in ssl
 //char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 512000\r\n\r\n";
 //int largeHttpBufSize = sizeof(largeBuf) + 512000 - 1;
 
-// fast in ssl
 //char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 5242880\r\n\r\n";
 //int largeHttpBufSize = sizeof(largeBuf) + 5242880 - 1;
 
-// fast in ssl
-//char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 52428800\r\n\r\n";
-//int largeHttpBufSize = sizeof(largeBuf) + 52428800 - 1;
+char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 52428800\r\n\r\n";
+int largeHttpBufSize = sizeof(largeBuf) + 52428800 - 1;
 
 //large response
 //char largeBuf[] = "HTTP/1.1 200 OK\r\nContent-Length: 104857600\r\n\r\n";
 //int largeHttpBufSize = sizeof(largeBuf) + 104857600 - 1;
 
-char *largeHttpBuf, *corkBuffer;
+char *largeHttpBuf;
 
-struct http_socket_ext {
-    int offset;
-};
+// us_wakeup_loop() triggers
+void on_wakeup(struct us_loop *loop) {
 
-struct http_context_ext {
-
-};
-
-void on_https_socket_writable(struct us_ssl_socket *s) {
-    // update offset!
-    us_ssl_socket_write(s, largeHttpBuf, largeHttpBufSize);
 }
 
-// keep polling for writable and assume the user will fill the buffer again
-// if not, THEN change the poll
 void on_http_socket_writable(struct us_socket *s) {
+    struct app_http_socket *http_socket = (struct app_http_socket *) us_socket_ext(s);
 
+    http_socket->offset += us_socket_write(s, largeHttpBuf + http_socket->offset, largeHttpBufSize - http_socket->offset, 0);
 }
 
 void on_http_socket_end(struct us_socket *s) {
-    //printf("Disconnected!\n");
+    printf("Disconnected!\n");
 }
 
-void on_https_socket_end(struct us_ssl_socket *s) {
-    //printf("Disconnected!\n");
+void on_http_socket_data(struct us_socket *s, void *data, int length) {
+    struct app_http_socket *http_socket = (struct app_http_socket *) us_socket_ext(s);
+
+    http_socket->offset = us_socket_write(s, largeHttpBuf, largeHttpBufSize, 0);
 }
 
+// we can get both socket and its context so user data is not needed!
 void on_http_socket_accepted(struct us_socket *s) {
-    printf("HTTP socket accepted!\n");
-}
+    struct app_http_socket *http_socket = (struct app_http_socket *) us_socket_ext(s);
 
-struct Header {
-    char *key, *value;
-    unsigned int keyLength, valueLength;
-};
-
-// UNSAFETY NOTE: assumes *end == '\r' (might unref end pointer)
-char *getHeaders(char *buffer, char *end, struct Header *headers, size_t maxHeaders) {
-    for (unsigned int i = 0; i < maxHeaders; i++) {
-        for (headers->key = buffer; (*buffer != ':') & (*buffer > 32); *(buffer++) |= 32);
-        if (*buffer == '\r') {
-            if ((buffer != end) & (buffer[1] == '\n') & (i > 0)) {
-                headers->key = 0;
-                return buffer + 2;
-            } else {
-                return 0;
-            }
-        } else {
-            headers->keyLength = (unsigned int) (buffer - headers->key);
-            for (buffer++; (*buffer == ':' || *buffer < 33) && *buffer != '\r'; buffer++);
-            headers->value = buffer;
-            buffer = (char *) memchr(buffer, '\r', end - buffer); //for (; *buffer != '\r'; buffer++);
-            if (buffer /*!= end*/ && buffer[1] == '\n') {
-                headers->valueLength = (unsigned int) (buffer - headers->value);
-                buffer += 2;
-                headers++;
-            } else {
-                return 0;
-            }
-        }
-    }
-    return 0;
-}
-
-void on_http_socket_data(struct us_socket *s, char *data, int length) {
-    data[length] = '\r';
-    #define MAX_HEADERS 100
-
-    struct Header headers[MAX_HEADERS];
-    if (getHeaders(data, data + length, headers, MAX_HEADERS)) {
-
-        // fill response
-        memcpy(corkBuffer, largeBuf, sizeof(largeBuf) - 1);
-        memcpy(corkBuffer + sizeof(largeBuf) - 1, largeHttpBuf, largeHttpBufSize - sizeof(largeBuf) + 1);
-
-        // send
-        us_socket_write(s, corkBuffer, largeHttpBufSize, 0);
-    } else {
-        printf("Got chunked HTTP headers\n");
-    }
-}
-
-void on_https_socket_accepted(struct us_ssl_socket *s) {
-    printf("HTTPS socket accepted!\n");
-}
-
-void on_https_socket_data(struct us_ssl_socket *s, char *data, int length) {
-    data[length] = '\r';
-    #define MAX_HEADERS 100
-
-    struct Header headers[MAX_HEADERS];
-    if (getHeaders(data, data + length, headers, MAX_HEADERS)) {
-
-        // fill response
-        memcpy(corkBuffer, largeBuf, sizeof(largeBuf) - 1);
-        memcpy(corkBuffer + sizeof(largeBuf) - 1, largeHttpBuf, largeHttpBufSize - sizeof(largeBuf) + 1);
-
-        // send
-        us_ssl_socket_write(s, corkBuffer, largeHttpBufSize);
-    } else {
-        printf("Got chunked HTTPS headers\n");
-
-        printf("Length: %d\n", length);
-    }
+    http_socket->offset = 0;
 }
 
 void on_http_socket_timeout(struct us_socket *s) {
-    printf("Dangit we timed out!\n");
 
-    // start timer again
-    us_socket_timeout(s, 2);
 }
 
-void on_https_socket_timeout(struct us_ssl_socket *s) {
-    printf("Dangit we timed out!\n");
-
-    // start timer again
-    us_ssl_socket_timeout(s, 2);
-}
-
-void timer_cb(struct us_timer *t) {
-    printf("Tick!\n");
-
-    us_wakeup_loop(us_timer_loop(t));
-}
-
-void on_wakeup(struct us_loop *loop) {
-    char *message = us_loop_ext(loop);
-    printf("%s\n", message);
-}
-
-void on_pre(struct us_loop *loop) {
-    //printf("Pre!\n");
-}
-
-void on_post(struct us_loop *loop) {
-    //printf("Post!\n");
-}
-
-// arg1 ska vara storleken på responsen, kör ett script över alla storlekar
 int main() {
-    // allocate some garbage data to send
+
     printf("%d\n", largeHttpBufSize);
+
     largeHttpBuf = malloc(largeHttpBufSize);
-    corkBuffer = malloc(largeHttpBufSize);
     memcpy(largeHttpBuf, largeBuf, sizeof(largeBuf) - 1);
 
-    // create a loop
-    char *message = "Tock!";
-    struct us_loop *loop = us_create_loop(on_wakeup, on_pre, on_post, strlen(message) + 1);
-    memcpy(us_loop_ext(loop), message, strlen(message) + 1);
+    // create the loop, and register a wakeup handler
+    struct us_loop *loop = us_create_loop(on_wakeup, on_wakeup, on_wakeup, 0); // shound take pre and post callbacks also!
 
-    // start a timer
-    struct us_timer *t = us_create_timer(loop, 0, 0);
-    //us_timer_set(t, timer_cb, 1000, 1000);
-
-    // create either an SSL socket context or a regular socket context
-#ifdef USE_SSL
-    struct us_ssl_socket_context_options options = {};
-    options.key_file_name = "/home/alexhultman/uWebSockets/misc/ssl/key.pem";
-    options.cert_file_name = "/home/alexhultman/uWebSockets/misc/ssl/cert.pem";
-    options.passphrase = "1234";
-
-    struct us_ssl_socket_context *http_context = us_create_ssl_socket_context(loop, sizeof(struct http_context_ext), options);
-
-    us_ssl_socket_context_on_open(http_context, on_https_socket_accepted);
-    us_ssl_socket_context_on_close(http_context, on_https_socket_end);
-    us_ssl_socket_context_on_data(http_context, on_https_socket_data);
-    us_ssl_socket_context_on_writable(http_context, on_https_socket_writable);
-    us_ssl_socket_context_on_timeout(http_context, on_https_socket_timeout);
-
-    struct us_listen_socket *listen_socket = us_ssl_socket_context_listen(http_context, 0, 3000, 0, sizeof(struct http_socket_ext));
-#else
-    struct us_socket_context *http_context = us_create_socket_context(loop, sizeof(struct http_context_ext));
+    // create a context (behavior) for httpsockets
+    struct us_socket_context *http_context = us_create_socket_context(loop, sizeof(struct app_http_context));
 
     us_socket_context_on_open(http_context, on_http_socket_accepted);
-    us_socket_context_on_close(http_context, on_http_socket_end);
     us_socket_context_on_data(http_context, on_http_socket_data);
     us_socket_context_on_writable(http_context, on_http_socket_writable);
+    us_socket_context_on_close(http_context, on_http_socket_end);
     us_socket_context_on_timeout(http_context, on_http_socket_timeout);
 
-    struct us_listen_socket *listen_socket = us_socket_context_listen(http_context, 0, 3000, 0, sizeof(struct http_socket_ext));
-#endif
+    // start accepting http sockets
+    struct us_listen_socket *listen_socket = us_socket_context_listen(http_context, 0, 3000, 0, sizeof(struct app_http_socket));
 
     if (listen_socket) {
         printf("Listening!\n");
