7a329b678f4f2582aaa3a59e18a4004fb0e9e3e7
Alex Hultman <alexhultman@Alexs-MBP.lan>
2019-08-25 04:42:13 +0200

    Fix a few GCD issues

--- src/eventing/gcd.c
+++ src/eventing/gcd.c
@@ -25,14 +25,21 @@
 struct us_loop_t *us_create_loop(void *hint, void (*wakeup_cb)(struct us_loop_t *loop), void (*pre_cb)(struct us_loop_t *loop), void (*post_cb)(struct us_loop_t *loop), unsigned int ext_size) {
     struct us_loop_t *loop = (struct us_loop_t *) malloc(sizeof(struct us_loop_t) + ext_size);
 
-    // init the queue
+    // init the queue from hint
 
     us_internal_loop_data_init(loop, wakeup_cb, pre_cb, post_cb);
+
+    // why not call integrate here? instead of in run
+
     return loop;
 }
 
 void us_loop_free(struct us_loop_t *loop) {
+    us_internal_loop_data_free(loop);
+    
+    // free queue if different from main
 
+    free(loop);
 }
 
 void us_loop_run(struct us_loop_t *loop) {
@@ -56,53 +63,70 @@ void us_poll_init(struct us_poll_t *p, LIBUS_SOCKET_DESCRIPTOR fd, int poll_type
     p->gcd_read = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, p->fd, 0, dispatch_get_main_queue());
     dispatch_set_context(p->gcd_read, p);
     dispatch_source_set_event_handler_f(p->gcd_read, gcd_read_handler);
+    //dispatch_activate(p->gcd_read);
 
     p->gcd_write = dispatch_source_create(DISPATCH_SOURCE_TYPE_WRITE, p->fd, 0, dispatch_get_main_queue());
     dispatch_set_context(p->gcd_write, p);
     dispatch_source_set_event_handler_f(p->gcd_write, gcd_write_handler);
+    //dispatch_activate(p->gcd_write);
 }
 
 void us_poll_free(struct us_poll_t *p, struct us_loop_t *loop) {
-
+    free(p);
 }
 
 void us_poll_start(struct us_poll_t *p, struct us_loop_t *loop, int events) {
     p->events = events;
 
     if (events & LIBUS_SOCKET_READABLE) {
-        dispatch_activate(p->gcd_read);
-    }
-
-    if (events & LIBUS_SOCKET_WRITABLE) {
-        dispatch_activate(p->gcd_write);
-    }
-}
-
-void us_poll_change(struct us_poll_t *p, struct us_loop_t *loop, int events) {
-
-    if (events & LIBUS_SOCKET_READABLE) {
         dispatch_resume(p->gcd_read);
-    } else {
-        dispatch_suspend(p->gcd_read);
     }
 
     if (events & LIBUS_SOCKET_WRITABLE) {
         dispatch_resume(p->gcd_write);
-    } else {
-        dispatch_suspend(p->gcd_write);
+    }
+}
+
+void us_poll_change(struct us_poll_t *p, struct us_loop_t *loop, int events) {
+    int old_events = p->events;
+
+    if ((old_events & LIBUS_SOCKET_READABLE) != (events & LIBUS_SOCKET_READABLE)) {
+        if (old_events & LIBUS_SOCKET_READABLE) {
+            dispatch_suspend(p->gcd_read);
+        } else {
+            dispatch_resume(p->gcd_read);
+        }
+    }
+
+    if ((old_events & LIBUS_SOCKET_WRITABLE) != (events & LIBUS_SOCKET_WRITABLE)) {
+        if (old_events & LIBUS_SOCKET_WRITABLE) {
+            dispatch_suspend(p->gcd_write);
+        } else {
+            dispatch_resume(p->gcd_write);
+        }
     }
 
     p->events = events;
 }
 
 void us_poll_stop(struct us_poll_t *p, struct us_loop_t *loop) {
+    if (p->events & LIBUS_SOCKET_READABLE) {
+        dispatch_suspend(p->gcd_read);
+    }
+
+    if (p->events & LIBUS_SOCKET_WRITABLE) {
+        dispatch_suspend(p->gcd_write);
+    }
+
     p->events = 0;
-    dispatch_suspend(p->gcd_read);
-    dispatch_suspend(p->gcd_write);
 }
 
 int us_poll_events(struct us_poll_t *p) {
     return p->events;
+}
+
+void *us_poll_ext(struct us_poll_t *p) {
+    return p + 1;
 }
 
 unsigned int us_internal_accept_poll_event(struct us_poll_t *p) {
@@ -128,8 +152,18 @@ struct us_poll_t *us_create_poll(struct us_loop_t *loop, int fallthrough, unsign
     return poll;
 }
 
+// still broken!
 struct us_poll_t *us_poll_resize(struct us_poll_t *p, struct us_loop_t *loop, unsigned int ext_size) {
-    return 0;
+    struct us_poll_t *new_p = realloc(p, sizeof(struct us_poll_t) + ext_size);
+    if (p != new_p) {
+        // does not seem to set these in time!
+        dispatch_set_context(new_p->gcd_read, new_p);
+        dispatch_set_context(new_p->gcd_write, new_p);
+
+        printf("Poll %p is now resized to poll %p\n", p, new_p);
+    }
+
+    return new_p;
 }
 
 /* Timers */
@@ -159,11 +193,9 @@ struct us_timer_t *us_create_timer(struct us_loop_t *loop, int fallthrough, unsi
 }
 
 void *us_timer_ext(struct us_timer_t *timer) {
-    //struct us_internal_callback_t *cb = (struct us_internal_callback_t *) timer;
+    struct us_internal_callback_t *cb = (struct us_internal_callback_t *) timer;
 
-    //return (cb + 1);
-
-    return 0;
+    return (cb + 1);
 }
 
 void us_timer_close(struct us_timer_t *t) {
@@ -182,25 +214,44 @@ void us_timer_set(struct us_timer_t *t, void (*cb)(struct us_timer_t *t), int ms
 }
 
 struct us_loop_t *us_timer_loop(struct us_timer_t *t) {
-    return 0;
+    struct us_internal_callback_t *internal_cb = (struct us_internal_callback_t *) t;
+
+    return internal_cb->loop;
 }
 
 /* Asyncs */
+void async_handler(void *c) {
+    struct us_internal_callback_t *internal_cb = (struct us_internal_callback_t *) c;
+
+    internal_cb->cb((struct us_internal_callback_t *) internal_cb->loop);
+}
+
 struct us_internal_async *us_internal_create_async(struct us_loop_t *loop, int fallthrough, unsigned int ext_size) {
-    printf("create async\n");
-    return 0;
+    struct us_internal_callback_t *cb = malloc(sizeof(struct us_internal_callback_t) + ext_size);
+
+    cb->loop = loop;
+    cb->cb_expects_the_loop = 1;
+
+    if (fallthrough) {
+        //uv_unref((uv_handle_t *) uv_timer);
+    }
+
+    return (struct us_internal_async *) cb;
 }
 
 void us_internal_async_close(struct us_internal_async *a) {
-
+    // cancel? free?
 }
 
 void us_internal_async_set(struct us_internal_async *a, void (*cb)(struct us_internal_async *)) {
+    struct us_internal_callback_t *internal_cb = (struct us_internal_callback_t *) a;
 
+    internal_cb->cb = (void (*)(struct us_internal_callback_t *)) cb;
 }
 
 void us_internal_async_wakeup(struct us_internal_async *a) {
-
+    // will probably need to track in-flight work item and cancel in close
+    dispatch_async_f(dispatch_get_main_queue(), a, async_handler);
 }
 
 #endif
