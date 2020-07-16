428d8a0a1e07f96cc8de5971e873a745a9ccf6ba
Jason Rhinelander <jason@imaginary.ca>
2020-07-16 17:37:08 -0300

    Fix compilation with mingw: Use lower-case includes
    Compiling for Windows on linux using mingw fails because of the
    capitalization in the Windows header includes (mingw provides only
    lower-case ones).  (Windows compilers on windows won't care about the
    case at all.)

--- src/libusockets.h
+++ src/libusockets.h
@@ -30,7 +30,7 @@
 /* Define what a socket descriptor is based on platform */
 #ifdef _WIN32
 #define NOMINMAX
-#include <WinSock2.h>
+#include <winsock2.h>
 #define LIBUS_SOCKET_DESCRIPTOR SOCKET
 #define WIN32_EXPORT __declspec(dllexport)
 #define alignas(x) __declspec(align(x))
