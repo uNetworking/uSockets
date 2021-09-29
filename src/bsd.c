0ebdde0601cc82349fc11a7c4bbb6dc5c9f28f42
Alex Hultman <alexhultman@gmail.com>
2021-09-29 05:21:37 +0200

    Never pass invalid send flags

--- src/bsd.c
+++ src/bsd.c
@@ -187,7 +187,7 @@ int bsd_send(LIBUS_SOCKET_DESCRIPTOR fd, const char *buf, int length, int msg_mo
 #ifdef MSG_MORE
 
     // for Linux we do not want signals
-    return send(fd, buf, length, (msg_more * MSG_MORE) | MSG_NOSIGNAL);
+    return send(fd, buf, length, ((msg_more != 0) * MSG_MORE) | MSG_NOSIGNAL);
 
 #else
 
